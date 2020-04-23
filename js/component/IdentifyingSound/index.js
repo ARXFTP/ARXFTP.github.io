498cc0106faf86f9afa88b43157f4fac904e51f4
ARXFTP <63627665+ARXFTP@users.noreply.github.com>
2020-04-23 12:21:11 +0800

    Add files via upload

--- js/component/IdentifyingSound/index.js
+++ js/component/IdentifyingSound/index.js
@@ -1,7 +1,7 @@
 var APPCODE = '5942591bb99a4bb8b0a9477a5a532b8c';
 // TODO 配置项里应该新增这个
 
-define(['polyfill'], function(MediaRecorder){
+define(['./polyfill'], function(MediaRecorder){
 
   var IdentifyingSound = {
     mediaRecorder: null,
