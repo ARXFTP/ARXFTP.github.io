1ad714a38ebe591e2885c1701d2fa71c07e6ef44
ARXFTP <63627665+ARXFTP@users.noreply.github.com>
2020-04-26 10:10:49 +0800

    Add files via upload

--- js/component/IdentifyingSound/index.js
+++ js/component/IdentifyingSound/index.js
@@ -2,93 +2,177 @@ var APPCODE = '5942591bb99a4bb8b0a9477a5a532b8c';
 // TODO 配置项里应该新增这个
 define([], function(){
 
-  var IdentifyingSound = {
-    mediaRecorder: null,
-    soundData: null,
-    init: function(successCallback, failedCallback){
-      this.successCallback = successCallback;
-      this.failedCallback = failedCallback;
-      this.initView();
-      this.bindEvent();
-    },
-    initView: function(){
-      API.openWebSocket();
-    },
-    bindEvent: function() {
-    },
-    startRecord: function(){
-      var that = this;
-      console.log(MediaRecorder);
-      navigator.mediaDevices.getUserMedia({ audio: true }).then(function(stream){
-        log("获取权限");
-        log("000000");
-        that.mediaRecorder = new MediaRecorder(stream);
-        n = ["start", "stop", "pause", "resume"]
-        log("1");
-        n.forEach(function(e) {
-          that.mediaRecorder.addEventListener(e, that.onEvent.bind(that, e))
-        }),
-        log("2");
-        that.mediaRecorder.addEventListener('dataavailable', that.onDataavailableEvent.bind(that))
-        that.mediaRecorder.addEventListener('stop', that.onStopEvent.bind(that))
-        log("3");
-        that.mediaRecorder.start();
-        log("4");
+var IdentifyingSound = {
+  mediaRecorder: null,
+  soundData: null,
+  init: function(successCallback, failedCallback){
+    var that = this;
+    that.successCallback = successCallback;
+    that.failedCallback = failedCallback;
+  },
+  isSupportRecord: function(){
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  startRecord: function(){
+    var that = this;
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(function(stream){
+      that.mediaRecorder = new MediaRecorder(stream);
+      that.mediaRecorder.addEventListener('dataavailable', that.onDataavailableEvent.bind(that))
+      that.mediaRecorder.addEventListener('stop', that.onStopEvent.bind(that))
+      that.mediaRecorder.start();
+
+      API.create_WebSocket();
+      API.bindEvent_WebSocketEvent({
+        openCallback: function(result){
+        },
+        closeCallback: function(result){
+        },
+        messageCallback: function(result){
+          that.successCallback && that.successCallback(result);
+          API.close_WebSocket();
+        },
+        errorCallback: function(result){
+          that.failedCallback && that.failedCallback('websocket error', result);
+        },
       });
-    },
-    onEvent: function(){
-      var that = this;
-      var mediaRecorder = this.mediaRecorder;
-      log(mediaRecorder.state);
-      log(mediaRecorder.mimeType);
-    },
-    onStopEvent: function(){
-      var that = this;
-      var mediaRecorder = this.mediaRecorder;
-      log(mediaRecorder.state);
-      log(mediaRecorder.mimeType);
-      that.successCallback && that.successCallback(that.soundData);
-      API.send(that.soundData);
-    },
-    onDataavailableEvent: function(e){
-      this.soundData = e.data
-    },
-    stopRecord: function(){
-      log(JSON.stringify(this));
-      log(this.mediaRecorder);
-      var mediaRecorder = this.mediaRecorder;
-      mediaRecorder.stop();
-      mediaRecorder.stream.getTracks().forEach(i => i.stop())
-    },
-    uploadSound: function(imgData, successCallback, failedCallback){
+    }).catch(function(){
+      that.failedCallback('录音功能异常');
+    });    
+  },
+  onStopEvent: function(){
+    var that = this;
+    API.send_WebSocket(that.soundData);
+    API.send_WebSocket('');
+  },
+  onDataavailableEvent: function(e){
+    this.soundData = e.data
+  },
+  stopRecord: function(){
+    var mediaRecorder = this.mediaRecorder;
+    mediaRecorder.stop();
+    mediaRecorder.stream.getTracks().forEach(i => i.stop())
+  }
+}  
+
+//  仅调通WebSocket接口 暂未调通POST stream/1接口
+var API = {
+  socket: null,
+  create_WebSocket: function (){
+    var socket = new WebSocket('wss://speech.xor-live.io/aquadaas/rest/speech/wsstream');
+    this.socket = socket;
+  },
+  bindEvent_WebSocketEvent:function(options){
+    var socket = this.socket;
+    socket.addEventListener('open', function (event) {
+      options.openCallback && options.openCallback(event)
+    });
+    socket.addEventListener('close', function (event) {
+      options.closeCallback && options.closeCallback(event)
+    });
+    socket.addEventListener('message', function (event) {
+      options.messageCallback && options.messageCallback(event)
+    });
+    socket.addEventListener('error', function (event) {
+      options.errorCallback && options.errorCallback(event)
+    });
+  },
+  send_WebSocket: function (soundPostData) {
+    this.socket.send(soundPostData);
+  },
+  close_WebSocket: function () {
+    this.socket.close();
+  },
+  sendWav: function(options, callback){
+    var that = this;
+    var url = 'http://speech.xor-live.io/aquadaas/rest/speech/stream/1?audiotype=pcm&audiorate=8000';
+    var formdata = new FormData(); // form 表单 {key:value}
+    //formdata.append("audio", options.data);
+    formdata.append("name", "file");
+    formdata.append("filename", options.data);
+    makeRequest({
+      url: url,
+      type: 'POST',
+      async: true,
+      timeout: 10000,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/octet-stream'
+      },
+      data: formdata,
+      done: function(raw){
+        callback && callback()
+      },
+      fail: function(xhr){
+        callback && callback()
+      }
+    });
+
+    function makeRequest (obj){
+      var type = obj.type || 'GET';
+      var url = obj.url;
+      var async = obj.async;
+      var done = obj.done;
+      var fail = obj.fail;
+      var always = obj.always;
+      var data = obj.data || null;
+      var headers = obj.headers;
+      var timeout = obj.timeout;
+      var xhr;
+      if(window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();
+      } else {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      xhr.open(type, url, async);
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+          if(xhr._timeout != null){
+            clearTimeout(xhr._timeout);
+            xhr._timeout = null;
+          }
+          if(xhr.status == 200 || xhr.status == 201){
+            if(typeof done === 'function'){
+              done(xhr.responseText);
+            }
+          } else {
+            if(typeof fail === 'function'){
+              fail(xhr);
+            }
+          }
+          if(typeof always === 'function'){
+            always(xhr);
+          }
+        }
+      };
+      // xhr.overrideMimeType("text/html;charset=utf-8");
+      if(headers){
+        for(var item in headers){
+          if(headers.hasOwnProperty(item)){
+            xhr.setRequestHeader(item, headers[item]);
+          }
+        }
+      }
+      if(timeout != null){
+        xhr._timeout = setTimeout(function(){
+          clearTimeout(xhr._timeout);
+          xhr._timeout = null;
+          xhr.abort();
+        }, timeout);
+      }
+      xhr.send(data);
+      return xhr;
+    }
+
+    function getTimeStamp(){
+      return (new Date().toISOString()).substr(0,19) + 'Z';
     }
   }
-  
-  var API = {
-    socket: null,
-    openWebSocket: function (successCallback, failedCallback){
-      let socket = new WebSocket('wss://speech.xor-live.io/aquadaas/rest/speech/wsstream');
-      socket.addEventListener('open', function (event) {
-        log('WebSocket open' + JSON.stringify(event));
-      });
-      socket.addEventListener('close', function (event) {
-        log('WebSocket close'+ JSON.stringify(event));
-      });
-      socket.addEventListener('message', function (event) {
-        log('Message from server '+ JSON.stringify(event));
-      });
-      socket.addEventListener('error', function (event) {
-        log('WebSocket error: '+ JSON.stringify(event));
-      });
-      this.socket = socket;
-    },
-    send: function (soundPostData) {
-      log(soundPostData);
-      log("WebSocket send");
-      this.socket.send(soundPostData);
-    },
-  };
+};
 
 
-  return IdentifyingSound;
+return IdentifyingSound;
 });
--- js/index_page.js
+++ js/index_page.js
@@ -1,109 +1,97 @@
 define(['js/utils/common.js',
-				'js/component/IdentifyingPhotos/index.js',
-				'js/component/IdentifyingSound/index.js',
-			], function(CommonApi, IdentifyingPhotos, IdentifyingSound){	
-	var indexPage = function(){};
-	indexPage.prototype = {
-		init: function(containerId){
-			var jq=jQuery;
-			jq("#"+containerId+"").append(`
-				<div style="border:1px red sold;width:100%;height:300px">
-					页面1
-					<button id='test'>上传图片</button>
-					<div id='log'></div>
+        'js/component/IdentifyingPhotos/index.js',
+        'js/component/IdentifyingSound/index.js',
+      ], function(CommonApi, IdentifyingPhotos, IdentifyingSound){	
+  var indexPage = function(){};
+  indexPage.prototype = {
+    init: function(containerId){
+      var jq=jQuery;
+      jq("#"+containerId+"").append(`
+        <div style="border:1px red sold;width:100%;height:300px">
+          页面1
+          <button id='test'>上传图片</button>
+          <div id='log'></div>
 
-					<button id="btnRecord">开始录制</button>
-					<button disabled id="btnPlay">开始播放</button>
-					<button disabled id="btnDownload">开始下载</button>
-					
-					<div id='log_'></div>
-					<div id='audioContainer'></div>
-				</div>
-			`);
-			jq("#"+containerId+"").append("<div id=\"next_page_button\" style=\"border:1px red sold;width:100%;height:150px\">跳下一页</div>");
-			var CommonApiObj = new CommonApi();
+          <button id="btnRecord">开始录制</button>
+          
+          <div id='log_'></div>
+          <div id='audioContainer'></div>
+        </div>
+      `);
+      jq("#"+containerId+"").append("<div id=\"next_page_button\" style=\"border:1px red sold;width:100%;height:150px\">跳下一页</div>");
+      var CommonApiObj = new CommonApi();
 
-			var logFlag = true;
+      var logFlag = true;
 
-			log("2020/4/24/14/36");
+      // 图片识别例子
+      var obj = Object.create(IdentifyingPhotos);
+      obj.init({
+        container: '#test'
+      }, function(data){
+        $('#log').html(JSON.stringify(data));
+      }, function(text){
+        $('#log').html(text);
+      });
 
-			// 图片识别例子
-			var obj = Object.create(IdentifyingPhotos);
-			obj.init({
-				container: '#test'
-			}, function(data){
-				$('#log').html(JSON.stringify(data));
-			}, function(text){
-				$('#log').html(text);
-			});
 
-			// 声音识别例子
-			var obj1 = Object.create(IdentifyingSound);
-			obj1.init(function(data){
-				var o = document.createElement("audio");
-				o.controls = !0;
-				o.src = URL.createObjectURL(data);
-				document.querySelector('div#audioContainer').appendChild(o)
-			}, function(){
+      // 声音识别例子
+      var obj1 = Object.create(IdentifyingSound);
 
-			})
+      if(obj1.isSupportRecord()){
+        obj1.init(function(data){
+          log(data.data);
+        }, function(result, error){
+          log(result);
+          log(JSON.stringify(error));          
+        })
+  
+        var btnRecord = document.querySelector('button#btnRecord');
+        btnRecord.onclick = function(){
+          if(btnRecord.textContent === '开始录制'){
+            obj1.startRecord();
+            btnRecord.textContent = '停止录制';
+            btnPlay.disabled = true;
+            btnDownload.disabled = true;
+          }else{
+            obj1.stopRecord();
+            btnRecord.textContent = '开始录制';
+            btnPlay.disabled = false;
+            btnDownload.disabled = false;
+          }
+        }
+      }else{
+        log('设备不支持');
+      }
 
-			var btnRecord = document.querySelector('button#btnRecord');
-			var btnDownload = document.querySelector('button#btnDownload');
-			btnRecord.onclick = () => {
-				if(btnRecord.textContent === '开始录制'){
-					obj1.startRecord();
-					btnRecord.textContent = '停止录制';
-					btnPlay.disabled = true;
-					btnDownload.disabled = true;
-				}else{
-					obj1.stopRecord();
-					btnRecord.textContent = '开始录制';
-					btnPlay.disabled = false;
-					btnDownload.disabled = false;
-				}
-			}
-			btnDownload.onclick = () => {
-				var a = document.createElement('a');
-				URL_ = URL.createObjectURL(obj1.soundData),
-				a.href = URL_;
-				a.download = 'test';
-				a.click();
-			}
-			
+      
 
 
 
-			window.onerror = function(msg, url, link){
-				log('报错信息', msg, {classList:['red'], focus:true});
-				log('报错行数', link, {classList:['red'], focus:true});
-			};
-			
-			function log(key, value, options) {
-				var log_ = document.getElementById('log_');
-				options = options || {};
-				var classList = options.classList || [];
-				var keyHtml = '<span style="font-weight: bolder">' + (key || '') + '</span>';
-				var outHtml = '<span class="' + classList.join(' ') + '">' + (value || '') + '</span>';
-				if(options.focus){
-					log_.innerHTML += keyHtml + ": " + outHtml + '<br>';
-					return;
-				}
-				if(logFlag){
-					log_.innerHTML += keyHtml + ": " + outHtml + '<br>';
-					return;
-				}
-			}
-			window.log = log;
-
-
-
-
-
-			document.getElementById("next_page_button").onclick=function(){
-				 CommonApiObj.togglePage('js/page_detail.js');
-			}
-		}
-	};
-	return indexPage
+      window.onerror = function(msg, url, link){
+        log('报错信息', msg, {classList:['red'], focus:true});
+        log('报错行数', link, {classList:['red'], focus:true});
+      };
+      
+      function log(key, value, options) {
+        var log_ = document.getElementById('log_');
+        options = options || {};
+        var classList = options.classList || [];
+        var keyHtml = '<span style="font-weight: bolder">' + (key || '') + '</span>';
+        var outHtml = '<span class="' + classList.join(' ') + '">' + (value || '') + '</span>';
+        if(options.focus){
+          log_.innerHTML += keyHtml + ": " + outHtml + '<br>';
+          return;
+        }
+        if(logFlag){
+          log_.innerHTML += keyHtml + ": " + outHtml + '<br>';
+          return;
+        }
+      }
+      window.log = log;
+      document.getElementById("next_page_button").onclick=function(){
+         CommonApiObj.togglePage('js/page_detail.js');
+      }
+    }
+  };
+  return indexPage
 })
